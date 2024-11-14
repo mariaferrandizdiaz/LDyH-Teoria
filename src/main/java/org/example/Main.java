6aba4a9ec4b5cb072346b36d526500efd3d2cffa
María Díaz <91562117+mariaferrandizdiaz@users.noreply.github.com>
2024-11-14 10:58:58 +0000

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(2 + 5);
+        System.out.println("ups");
     }
 }
