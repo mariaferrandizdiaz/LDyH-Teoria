6fddfef3517d3d468140ff24210217dd9aea4e70
María Díaz <91562117+mariaferrandizdiaz@users.noreply.github.com>
2024-11-14 10:49:36 +0000

    upload hello world proyect

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Package example.
+ */
+package org.example;
+
+/**
+ * Main class: Dice "Hello world!" como modo de prueba.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
