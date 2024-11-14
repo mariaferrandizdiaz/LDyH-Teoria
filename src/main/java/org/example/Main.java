a421d0218c957debe2ee04c6d51829c72d21a903
María Díaz <91562117+mariaferrandizdiaz@users.noreply.github.com>
2024-11-14 11:01:34 +0000

    Adaptar al Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,18 @@
  */
 package org.example;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+
 /**
  * Main class: Dice "Hello world!" como modo de prueba.
  */
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("ups");
+        logger.log(Level.WARNING, "ups");
     }
 }
